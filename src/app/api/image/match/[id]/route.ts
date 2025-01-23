674748506887a5b970e7c93cf05ff496870a830f
zterhes <zterhes1@gmail.com>
2025-01-23 21:14:42 +0100

    format by hand

--- src/app/api/image/match/[id]/route.ts
+++ src/app/api/image/match/[id]/route.ts
@@ -1,4 +1,4 @@
-import { generatedImagesPostsTable, generatedImagesTable, postsTable } from "@/db/schema";
+import { generatedImagesTable } from "@/db/schema";
 import { handleAuth } from "@/lib/auth";
 import { handleError } from "@/lib/utils";
 import { db } from "@/db";
